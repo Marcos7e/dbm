d8263f2bc8ae9909fb3be9d93a980e7a09d59aae
Marcos Barrera <mmbarrerae@outlook.es>
2018-02-16 21:30:43 -0600

    modales proceso masivo

--- blog/resources/views/migration.blade.php
+++ blog/resources/views/migration.blade.php
@@ -4,6 +4,50 @@
 
 @section('content')
 
+<!-- Modal -->
+<div class="modal fade" id="processsMdl" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Procesar Archivo Masivo</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        Esta seguro de ejecutar el archivo seleccionado?
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-success">Ejecutar</button>
+          <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancelar</button>
+      </div>
+    </div>
+  </div>
+</div>
+<!--fin de modal-->
+
+<!-- Modal -->
+<div class="modal fade" id="deleteMdl" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Procesar Archivo Masivo</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        Esta seguro de ejecutar el archivo seleccionado?
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-danger">Eliminar</button>
+          <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancelar</button>
+      </div>
+    </div>
+  </div>
+</div>
+<!--fin de modal-->
+
 <div class="container">
   <div class="row">
     <div class="jumbotron">
@@ -29,7 +73,7 @@
         @foreach ($files as $file)
         <tr>
           <th>{{$file}}</th>
-          <th><button type="button"class="btn btn-success" onclick="url('/welcome')">Procesar</button>  <button type="button" class="btn btn-danger">Eliminar</button></th>
+          <th><button type="button"class="btn btn-success"  data-toggle="modal" data-target="#processsMdl">Procesar</button>  <button type="button" class="btn btn-danger" data-toggle="modal" data-target="#deleteMdl">Eliminar</button></th>
         </tr>
         @endforeach
       </tbody>
